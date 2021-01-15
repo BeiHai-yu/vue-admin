a89413c233d28f3ab2dbdd3efd3af92525737149
BeiHai-yu <1634586466@qq.com>
2021-01-15 17:26:50 +0800

    项目初始化-调整初始l目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+// 项目启动入口
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 记载全局样式文件
+import './styles/index.less'
+// 创建vue根实例
+// 把router配置到根实例中
+// 通过render方法把app跟组件渲染到
 createApp(App).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,12 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter)
 
-const router = createRouter({
-  history: createWebHashHistory(),
+// 路由配置表
+const routes = []
+
+const router = new VueRouter({
   routes
 })
 
